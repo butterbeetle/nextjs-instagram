f834046e1933d8f7dcac6baa962ba06331f5a3a7
butterbeetle <butterbeetle@naver.com>
2023-08-09 22:06:44 +0900

    Fix. 로그인 없이 프로필 페이지 못오게 변경

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,8 +1,18 @@
+import { getServerSession } from "next-auth";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
 type Props = {
   params: {
     username: string;
   };
 };
-export default function UserPage({ params: { username } }: Props) {
+export default async function UserPage({ params: { username } }: Props) {
+  const session = await getServerSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
   return <p>{username}</p>;
 }
