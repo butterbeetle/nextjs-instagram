8e16facd03163bc44af4b5e0be8d9167df31549c
butterbeetle <butterbeetle@naver.com>
2023-08-11 21:01:34 +0900

    Feat. Post 가져와서 보여주기

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,8 +1,14 @@
 "use client";
+import { SimplePost } from "@/model/post";
+import Image from "next/image";
 import useSWR from "swr";
 
 export default function PostList() {
-  const { data, isLoading, error } = useSWR("/api/posts");
-  console.log(data);
-  return <p>Post List</p>;
+  const { data: posts, isLoading, error } = useSWR<SimplePost[]>("/api/posts");
+  console.log(posts);
+  return (
+    <section>
+      <ul>{posts && posts.map(({ id, text }) => <li key={id}>{text}</li>)}</ul>
+    </section>
+  );
 }
--- src/service/posts.ts
+++ src/service/posts.ts
@@ -4,7 +4,17 @@ export async function getFollowingPostsOf(username: string) {
   return client.fetch(
     `*[_type == "post" && username == "${username}" 
     || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
-    | order(_createdAt desc)
+    | order(_createdAt desc){
+      ...,
+      "username": author->username,
+      "userImage": author->image,
+      "image": photo,
+      "likes": likes[]->username,
+      "text": comments[0].comment,
+      "comments": count(comments),
+      "id":_id,
+      "createdAt":_createdAt
+    }
     `
   );
 }
