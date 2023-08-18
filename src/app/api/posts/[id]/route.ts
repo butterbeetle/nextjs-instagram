083e98d3f55371211aa739433fe5cd2da048a453
butterbeetle <butterbeetle@naver.com>
2023-08-18 21:33:34 +0900

    Refactor. 세션쪽 리팩토링

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,7 +1,6 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
-import { getFollowingPostsOf, getPost } from "@/service/posts";
+import { getPost } from "@/service/posts";
 import { NextRequest, NextResponse } from "next/server";
+import { withSessionUser } from "@/util/session";
 
 type Context = {
   params: {
@@ -10,13 +9,8 @@ type Context = {
 };
 
 export async function GET(_: NextRequest, context: Context) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) {
-    return new Response("Authentication Error", { status: 401 });
-  }
-
-  return getPost(context.params.id) //
-    .then((data) => NextResponse.json(data));
+  return withSessionUser(async () =>
+    getPost(context.params.id) //
+      .then((data) => NextResponse.json(data))
+  );
 }
